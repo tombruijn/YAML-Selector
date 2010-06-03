3f280d75e04faed2b762e993229b6d9bfdefc5a5
Tom de Bruijn <tom@newanz.com>
2010-06-03 15:43:13 +0200

    Variable replacement and some styling to the example page.

--- index.php
+++ index.php
@@ -1,35 +1,88 @@
 <?php
 require_once("selector.php");
-echo "<html>
+echo "<!DOCTYPE html>
+<html>
 <head>
+	<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
 	<title>Yaml Selector examples</title>
 	<style type='text/css'>
 		body{
 			margin: 10px;
+			font-family: Arial, Verdana;
+			font-size: 90%;
 		}
 		p,pre{
 			margin: 0px;
 		}
 		
 		h1,h2,h3,h4{
-			margin: 0;
+			margin: 0 0 5px 0;
+		}
+		h1{
+			font-size: 20px;
+			color: #00a2ff;
+			border-bottom: 1px solid #00a2ff;
+		}
+		h2{
+			font-size: 16px;
+			color: #FF8A00;
+			border-bottom: 1px solid #FF8A00;
+		}
+		h3{
+			font-size: 14px;
+			color: #6CADC0;
+			border-bottom: 1px solid #6CADC0;
+		}
+		h4{
+			font-size: 12px;
+			color: #323232;
+		}
+		.content{
+			margin-right: 360px;
+			min-width: 500px;
+			background-color: #FFF;
+		}
+		.example_file{
+			position: fixed;
+			top: 10px;
+			right: 10px;
+			float: right;
+			width: 330px;
+			padding: 10px;
+			background-color: #FFF;
+			border: 1px solid #CCC;
+		}
+		.example{
+			margin-bottom: 10px;
+			padding: 10px 10px 10px 10px;
+			border: 1px solid #CCC;
+			border-radius: 10px;
+			-moz-border-radius: 10px;
+			-webkit-border-radius: 10px;
+		}
+		.example code{
+			display: block;
+			margin-bottom: 10px;
 		}
 	</style>
 </head>
 <body>";
 $yaml = new YamlSelector("data.yaml");
-echo "<h1>".$yaml->get("yaml.selector.name")."</h1>";
-echo "<p>".$yaml->get("yaml.selector.description")."</p>";
-echo "<h2>Usage</h2>";
-echo "<h3>Example file</h3>";
-highlight_file("example_file.yaml");
+echo "<div class='example_file'>
+	<h2>Example file</h2>
+	".highlight_file("example_file.yaml",true)."
+</div>
+<div class='content'>
+<h1>".$yaml->get("yaml.selector.name")."</h1>
+<p>".$yaml->get("yaml.selector.description")."</p>
+<h2>Usage</h2>";
 $examples = $yaml->get("yaml.selector.example");
 foreach($examples as $example){
-	echo "<h3>".$example["name"]."</h3>";
+	echo "<div class='example'><h3>".$example["name"]."</h3>";
 	echo "<h4>Code</h4>";
 	highlight_string("<?php\n".$example["code"]);
 	echo "<h4>Result</h4>";
-	echo "<p>".$example["result"]."</p>";
+	echo "<p>".$example["result"]."</p></div>";
 }
 $e = new YamlSelector("example_file.yaml");
 echo "<h2>3rd party library</h2>";
@@ -39,5 +92,7 @@ $authors = $yaml->get("yaml.library.author");
 foreach($authors as $k=>$v){
 	echo "<li>".$k." (".$v["email"].")</li>";
 }
-echo "</ul></p>";
-echo "<body></html>";
+echo "</ul></p>
+</div>
+</body>
+</html>";
--- selector.php
+++ selector.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Yaml Selector. Simple level selection class for YAML files.
-@version 0.0.1
+@version 0.0.2
 @author Tom de Bruijn <tom@newanz.com>
 @link 
 @copyright Copyright 2010 Tom de Bruijn
@@ -13,6 +13,7 @@ Yaml Selector. Simple level selection class for YAML files.
 class YamlSelector{
 	private $data = array(); #Used to store the entire selected YAML file.
 	private $setting = array("type"=>"array"); #Settings for return values.
+	static $variables = array(); #Very rapidly changing values here.
 	/*
 	__construct
 	@param String Optional $file = Path to the YAML file you want to load.
@@ -42,20 +43,21 @@ class YamlSelector{
 			- firstlevel
 			- firstlevel.secondlevel
 			- firstlevel.secondlevel.thirdlevel
+	@param Array $variables = Keys are name of the variables in the YAML variable. Array values are what they will be replaces with.
 	@return mixed; String/Array, depending on the selection level.
 	*/
-	function get($selector){
+	function get($selector,$variables=array()){
 		$keys = explode(".",$selector);
 		$var = $this->data;
 		foreach($keys as $key){
 			$v = $this->returnValue($var,$key);
-			if(!is_array($v)){
-				return $v;
-			}
 			$var = $v;
 		}
 		if($this->setting["type"]=="object"){
 			$var = (object)$var;
+		}
+		if(is_string($var) && !empty($variables)){
+			$var = $this->parseVariables($var,$variables);
 		}
 		return $var;
 	}
@@ -73,5 +75,19 @@ class YamlSelector{
 		}else{
 			return false;
 		}
+	}
+	/*
+	parseVariables
+	
+	Private method
+	@param Array $var = Array with variables.
+	@param String $key = Key in the array.
+	@return: mixed; String.
+	*/
+	private function parseVariables($var,$variables){
+		self::$variables = $variables; //Egh...
+		return preg_replace_callback("#{{(.*?)}}#is",
+			create_function('$string','return YamlSelector::$variables[$string[1]];')
+		,$var);
 	}
 }
